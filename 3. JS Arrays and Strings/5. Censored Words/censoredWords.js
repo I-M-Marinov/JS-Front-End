c92235f803d864b4d6530c6076cffb6e6190cc13
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2024-02-27 19:09:54 +0200

    Exercises 5 and 6

--- 3. JS Arrays and Strings/5. Censored Words/censoredWords.js
+++ 3. JS Arrays and Strings/5. Censored Words/censoredWords.js
@@ -3,3 +3,14 @@ Write a function that receives a text as a first parameter and a single word as 
 Find all occurrences of that word in the text and replace them with the corresponding count of '*'.
 */
 
+function censor(sentance,word){
+
+    while (sentance.includes(word)){
+        sentance = sentance.replace(word, "*".repeat(word.length));
+    }
+
+    console.log(sentance);
+}
+
+censor('A small sentence with some words', 'small');
+censor('Find the hidden word', 'hidden');
