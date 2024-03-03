e6a991afa7d98537620a7e396a1b60c03b5dd998
I-M-Marinov <lcfrrr@gmail.com>
2024-03-03 11:54:29 +0200

    2. Math Power solution

---
+++ 5. Functions and Statements - Lab/2. Math Power/mathPower.js
@@ -0,0 +1,14 @@
+/* 
+Write a function that calculates and print the value of a number raised to a given power:
+
+Examples
+            Input	Output
+            2,8	    256
+            3,4	    81
+
+*/
+
+function solve(number,power) { console.log(number ** power); }
+
+solve(2,8);
+solve(3,4);
