4f7a4d010a04e832115677287f7ab01c494511af
I-M-Marinov <lcfrrr@gmail.com>
2024-03-14 18:45:47 +0200

    add exercise 5

---
+++ 8. Objects and Classes - Exercise/5. Inventory/inventory.js
@@ -0,0 +1,16 @@
+/* 
+Create a function, which creates a register for heroes, with their names, level, and items (if they have such). 
+The input comes as an array of strings. Each element holds data for a hero, in the following format:
+
+"{heroName} / {heroLevel} / {item1}, {item2}, {item3}..." 
+
+You must store the data about every hero. The name is a string, a level is a number and the items are all strings.
+The output is all of the data for all the heroes you’ve stored sorted ascending by level. The data must be in the following format for each hero:
+
+Hero: {heroName}
+level => {heroLevel}
+Items => {item1}, {item2}, {item3}
+
+*/
+
+
