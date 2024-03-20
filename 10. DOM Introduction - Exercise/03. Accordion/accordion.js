ffaeaa369b469b0447638b7adfce1924c9c2c611
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2024-03-20 16:28:39 +0200

    Solutions for Exercise 3 Accordion and Exercise 4 Serach In List

--- 10. DOM Introduction - Exercise/03. Accordion/accordion.js
+++ 10. DOM Introduction - Exercise/03. Accordion/accordion.js
@@ -1,3 +1,36 @@
+/* 
+An HTML file is given and your task is to show more/less information. 
+By clicking the [More] button, it should reveal the content of a hidden div and changes the text of the button to [Less]. 
+When the same link is clicked again (now reading Less), hide the div and change the text of the link to More.
+Link action should be toggleable (you should be able to click the button an infinite amount of times).
+
+*
+*************************************************Hints*************************************************
+•	To change the text content of a button, you could use getElementsByClassName. 
+However, that returns a collection and we need only one element from it, 
+so the correct way is to use getElementsByClassName("button")[0] as it will return the needed span element.
+
+•	After that, we should change the display style of the div with an id "extra". 
+If the display style is "none", we should change it to "block" and the opposite.
+
+•	Along with all of this, we should change the text content of the button to [Less]/[More].
+
+*/
+
 function toggle() {
-    console.log('TODO:...');
-}
+
+    const buttonElement = document.getElementsByClassName("button")[0];
+    const extraTextElement = document.getElementById("extra");
+
+    if ( buttonElement.textContent == "Less"){
+        extraTextElement.style.display = 'none';
+        buttonElement.textContent = "More";
+    } else {
+        extraTextElement.style.display = 'block';
+        buttonElement.textContent = "Less";
+    }
+    
+        console.log(buttonElement.textContent);
+        console.log(extraTextElement);
+        console.log(buttonElement);
+    }
--- 10. DOM Introduction - Exercise/04. Search in List/search.js
+++ 10. DOM Introduction - Exercise/04. Search in List/search.js
@@ -1,3 +1,35 @@
+/* 
+An HTML page holds a list of towns, a search box, and a [Search] button. 
+Implement the search function to bold and underline the items from the list which include the text from the search box. 
+Also, print the number of items the current search matches in the format `${matches} matches found`.
+Note: It is necessary to clear the results of the previous search.
+Write your JavaScript code in this file:
+*/
+
 function search() {
-   // TODO
+   const searchText = document.getElementById("searchText").value.toLowerCase();
+   const listItems = document.getElementById("towns").getElementsByTagName("li");
+   let matches = 0;
+
+   for (var i = 0; i < listItems.length; i++) {
+       listItems[i].innerHTML = listItems[i].textContent;
+       listItems[i].style.fontWeight = 'normal';
+       listItems[i].style.textDecoration = 'none';
+   }
+
+   for (var i = 0; i < listItems.length; i++) {
+       var itemText = listItems[i].textContent.toLowerCase();
+       if (itemText.includes(searchText)) {
+           matches++;
+           let index = itemText.indexOf(searchText);
+           let highlightedText = listItems[i].innerHTML.substring(0, index) +
+               '<span class="highlight">' + listItems[i].innerHTML.substring(index, index + searchText.length) + '</span>' +
+               listItems[i].innerHTML.substring(index + searchText.length);
+           listItems[i].innerHTML = highlightedText;
+           listItems[i].style.fontWeight = 'bold';
+           listItems[i].style.textDecoration = 'underline';
+       }
+   }
+   
+   document.getElementById("result").innerHTML = matches + " matches found";
 }
