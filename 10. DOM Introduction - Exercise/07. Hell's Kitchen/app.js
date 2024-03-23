17bf7001d99f9db8d8f5e128cbbb650be2d9cdc1
I-M-Marinov <lcfrrr@gmail.com>
2024-03-23 15:58:20 +0200

    Solution to Exercise 8 Generate  Report

--- 10. DOM Introduction - Exercise/07. Hell's Kitchen/app.js
+++ 10. DOM Introduction - Exercise/07. Hell's Kitchen/app.js
@@ -1,8 +1,38 @@
+/* 
+You will be given an array of strings, which represents a list of all the restaurants with their workers.
+
+When the [Send] button is clicked:
+•	Display the best restaurant of all the added restaurants with its average salary and best salary. 
+•	If there is a restaurant in the input array that is added more than once, you need to add new workers to the old ones and update the values of the average salary and the best salary.
+•	The best restaurant is the restaurant with the highest average salary. If two restaurants have the same average salary the best restaurant is the first one added.
+•	Display all workers in the best restaurant with their salaries.
+The best restaurant's workers should be sorted by their salaries in descending order.
+
+Input
+The input will be received from the given textarea in the form of an array of strings. 
+Each string represents a restaurant with its workers: ["Mikes - Steve 1000, Ivan 200, Paul 800", "Fleet - Maria 850, Janet 650"]
+
+Output
+•	The output contains two strings
+o	The first one is the best restaurant in the format:
+`Name: {restaurant name} Average Salary: {restaurant avgSalary} Best Salary: {restaurant bestSalary}`
+avgSalary and bestSalary must be formatted to the second decimal point.
+o	The second one is all the workers in that restaurant in the following format:
+`Name: {worker name} With Salary: {worker salary} Name: {worker2 name} With Salary: {worker2 salary} Name: {worker3 name} With Salary: {worker3 salary}…`
+
+Output strings must be set like text content in the following elements:
+
+Constraints
+•	The workers will be always unique
+
+
+*/
+
 function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
    function onClick () {
-      //   TODO:
+         
       
    }
 }
--- 10. DOM Introduction - Exercise/08. Generate Report/report.js
+++ 10. DOM Introduction - Exercise/08. Generate Report/report.js
@@ -1,3 +1,53 @@
+/* 
+You will be given a web page, containing a table and output area. 
+
+When the "Generate Report" button is pressed:
+•	You must generate a JSON report from the data inside the table, by only taking the columns, which are selected.
+Each table header has a checkbox. If the checkbox is checked, then the data from this column must be included in the report. Unchecked columns must be omitted. 
+
+For every row (excluding the header):
+•	Create an object with properties for each of its columns.
+•	The name of each property is the name attribute of the column’s header, and the value is the text content of the cell.
+•	Store the result in an array and output it as a JSON string display it inside the <textarea> with id "output". See the example for details.
+
+Input/Output
+There will be input, your program must execute based on the page content. The output must be a JSON string, displayed in the <textarea> with id "output".
+
+*/
+
 function generateReport() {
-    //TODO
+    const thElements = document.querySelectorAll('table thead th');
+    const trElements = document.querySelectorAll('table tbody tr');
+    const outputElement = document.getElementById('output');
+
+    const columns = Array
+    .from(thElements)
+    .map((thElement) => {
+        const checkboxElement = thElement.querySelector('input[type=checkbox]');
+        return {
+            name: thElement.textContent.toLowerCase().trim(),
+            active: checkboxElement.checked,
+        };
+    });
+
+    const reportData = Array
+    .from(trElements)
+    .map(trElement => {
+        const tdElements = trElement.querySelectorAll('td');
+
+        const result = Array.from(tdElements)
+        .reduce((data, tdElement, i) => {
+                if (!columns[i].active){
+                    return data;
+                }
+                const columnName = columns[i].name;
+                data[columnName] = tdElement.textContent;
+
+                return data;
+        }, {});
+
+        return result;
+    });
+
+    outputElement.value = JSON.stringify(reportData), null, 2;
 }
