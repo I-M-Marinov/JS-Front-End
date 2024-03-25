b3903730b230d90dccc3b4bfe1195d0daf66ecc3
I-M-Marinov <lcfrrr@gmail.com>
2024-03-25 15:05:35 +0200

    Exercise 1. List of Items Solution

--- 11. DOM Manipulation and Events - Lab/01. List-Of-Items/app.js
+++ 11. DOM Manipulation and Events - Lab/01. List-Of-Items/app.js
@@ -1,3 +1,15 @@
+/* 
+Write a function that reads the text inside an input field and appends the specified text to a list inside an HTML page.
+*/
+
 function addItem() {
-    console.log('TODO:...');
+
+    const listOfItemsElements = document.querySelector('#items'); // Get the list of elements with id "items"
+    let newItemText = document.getElementById('newItemText').value; // Get the value of the newItemText input field with id "newItemText"
+
+    let li = document.createElement("li"); // Create list item
+    li.textContent = newItemText; // Add the text from the newItemText input field
+    listOfItemsElements.appendChild(li); // Append it to the ul list of elements
+    newItemText = document.getElementById('newItemText').value = ''; // Clear the value of the newItemText
+
 }
