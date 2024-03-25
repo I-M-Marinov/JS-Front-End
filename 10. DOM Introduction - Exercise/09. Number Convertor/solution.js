34afe8e37e7213a689a4fa3c8abde2a34cd7a143
I-M-Marinov <lcfrrr@gmail.com>
2024-03-25 14:39:08 +0200

    Added the resources for the DOM Events Lab

--- 10. DOM Introduction - Exercise/09. Number Convertor/solution.js
+++ 10. DOM Introduction - Exercise/09. Number Convertor/solution.js
@@ -1,15 +1,11 @@
 /* 
 Write a function that converts a decimal number to binary and hexadecimal.
-
 The given number will always be in decimal format. The "From" select menu will only have a Decimal option,
 but the "To" select menu will have two options: Binary and Hexadecimal. 
 This means that our program should have the functionality to convert decimal to binary and decimal to hexadecimal. 
 When you convert to hexadecimal it must be upper case.
 Note that the "To" select menu by default is empty. You have to insert the two options ('Binary' and 'Hexadecimal') inside before continuing. 
 Also, they should have values ('binary' and 'hexadecimal').
-
-
-
 */
 
 function solve() {
