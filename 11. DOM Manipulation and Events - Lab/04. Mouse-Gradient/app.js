edbbf35d98365676a110ebd1d339ac3b5e26ec06
I-M-Marinov <lcfrrr@gmail.com>
2024-03-27 15:32:41 +0200

    Solution to Ex. 5 Highlight Active

--- 11. DOM Manipulation and Events - Lab/04. Mouse-Gradient/app.js
+++ 11. DOM Manipulation and Events - Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,11 @@
+/* 
+Write a program that detects and displays how far along a gradient the user has moved their mouse. 
+The result should be rounded down and displayed as a percentage inside the <div> with id "result". 
+
+Input/Output
+There will be no input/output, your program should instead modify the DOM of the given HTML document.
+*/
+
 function attachGradientEvents() {
     
     const gradientElement = document.getElementById('gradient');
--- 11. DOM Manipulation and Events - Lab/05. Highlight-Active/app.js
+++ 11. DOM Manipulation and Events - Lab/05. Highlight-Active/app.js
@@ -1,3 +1,23 @@
+/* 
+Write a function that highlights the currently active section of a document. 
+There will be multiple divs with input fields inside them. Set the class of the div that contains the currently focused input field to "focused".
+When the focus is lost (blurred), remove the class from the element.
+
+Input/Output
+There will be no input/output, your program should instead modify the DOM of the given HTML document.
+*/
+
 function focused() {
-    console.log('TODO:...');
+    
+    const inputElements = document.querySelectorAll('input[type=text]');
+    const inputElementsArray = Array.from(inputElements);
+
+    inputElementsArray.forEach(inputElement => inputElement.addEventListener('focus', (event) => {
+        event.target.parentElement.classList.add('focused');
+    }));
+
+    inputElementsArray.forEach(inputElement => inputElement.addEventListener('blur', (event) => {
+        event.target.parentElement.classList.remove('focused');
+    }));
+    
 }
