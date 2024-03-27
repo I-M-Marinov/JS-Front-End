98c5c55cf1d87ffc5deb462ce4ce6f83d9c3cacf
I-M-Marinov <lcfrrr@gmail.com>
2024-03-27 15:44:28 +0200

    Solution to Ex. 6 Dynamic-Validation

--- 11. DOM Manipulation and Events - Lab/06. Dynamic-Validation/app.js
+++ 11. DOM Manipulation and Events - Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,23 @@
+/* 
+Write a function that dynamically validates an email input field when it is changed. If the input is invalid, apply the class "error".
+Do not validate on every keystroke, as it is annoying for the user, consider only change events.
+A valid email is considered to be in the format: <name>@<domain>.<extension>
+Only lowercase Latin characters are allowed for any of the parts of the email. If the input is valid, clear the style. Submit only the validate() function in Judge.
+
+Input/Output
+There will be no input/output, your program should instead modify the DOM of the given HTML document.
+*/
+
 function validate() {
-    console.log('TODO:...');
+    
+    const emailInputElement = document.getElementById('email');
+    const pattern = /^[a-z]+@[a-z]+\.[a-z]+/;
+
+    emailInputElement.addEventListener('change', (event) => {
+        if (!pattern.test(event.target.value)){
+            event.target.classList.add('error');
+        } else {
+            event.target.classList.remove('error');
+        }
+    });
 }
