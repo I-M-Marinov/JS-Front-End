35cc622676586c77f888b6adec2b9668abc18eda
I-M-Marinov <lcfrrr@gmail.com>
2024-04-05 18:05:05 +0300

    Solution to Ex.2 Bus-Schedule

--- 14-Asynchronous-Programming/02.Bus-Schedule/app.js
+++ 14-Asynchronous-Programming/02.Bus-Schedule/app.js
@@ -1,11 +1,48 @@
 function solve() {
-    function depart() {
-        // TODO: 
-    }
 
+    const targetUrl = "http://localhost:3030/jsonstore/bus/schedule";
+    const departButtonElement = document.getElementById('depart');
+    const arriveButtonElement = document.getElementById('arrive');
+    const infoElement = document.querySelector('.info');
+    let currentStop = 'depot'; // default is depot
+    let nextStop = ''; 
+
+    async function depart() {
+
+            fetch(`${targetUrl}/${currentStop}`)
+            .then(res => res.json())
+            .then(currentStopData => {
+                infoElement.textContent = `Next stop ${currentStopData.name}`;
+                nextStop = currentStopData.next;
+            })
+            .catch(error => console.log(error));       
+        }
+
+        departButtonElement.addEventListener('click', () => {
+
+            departButtonElement.setAttribute("disabled", "true");
+            arriveButtonElement.removeAttribute("disabled");
+
+        });
+    
     async function arrive() {
-        // TODO:
-    }
+
+            fetch(`${targetUrl}/${currentStop}`)
+            .then(res => res.json())
+            .then(arrivedStopData => {
+                infoElement.textContent = `Arriving at ${arrivedStopData.name}`;
+                currentStop = nextStop;
+            })
+            .catch(error => console.log(error));
+        };
+
+        arriveButtonElement.addEventListener('click', () => {
+
+            arriveButtonElement.setAttribute("disabled", "true");
+            departButtonElement.removeAttribute("disabled");
+
+        });
+
 
     return {
         depart,
